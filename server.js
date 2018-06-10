5247a5e85a534ad433420a639ed3996476cdd4f0
tarikporto <tariklemos1511@gmail.com>
2018-06-10 19:31:17 -0300

    - alinhamento da home

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
-app.listen(3030);
+app.listen(port);
 
 console.log("bibSis RESTful API server started on: " + port);
 
